2243f4f436289123ac79e51e9b418a7e8e04897c
Anmol Budhiraja <anmolbudhiraja1999@gmail.com>
2024-09-25 23:44:11 -0700

    Add Starter Code for PA2

---
+++ src/Tool.java
@@ -0,0 +1,22 @@
+public class Tool {
+
+    /**
+     * Calculates the mean of a Series object.
+     *
+     * @param s the Series object to calculate the mean from
+     */
+    public static double mean(Series s) throws ArithmeticException {
+        // TODO: Implement mean calculation
+        return 0.0;
+    }
+
+    /**
+     * Finds the maximum value in a Series object.
+     *
+     * @param s the Series object to find the maximum value from
+     */
+    public static Integer max(Series s) throws IllegalArgumentException {
+        // TODO: Implement max value finder
+        return null;
+    }
+}
